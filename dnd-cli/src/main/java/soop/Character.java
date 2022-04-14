795e790a031965264a92d683608b9904537e1e99
soopstar <probablysoup@protonmail.com>
2022-04-14 13:59:20 -0400

    created initial Character and Item classes

--- dnd-cli/src/main/java/soop/Character.java
+++ dnd-cli/src/main/java/soop/Character.java
@@ -1,5 +1,46 @@
 package soop;
 
+import java.util.ArrayList;
+
 public class Character {
+    private final String name;
+    private final int level;
+    private final String race;
+    private final String background;
+    private final String align;
+    private final String charClass;
+    private final int hitDice;
+
+    // Stats
+    private int hp;
+    private final int hpMax;
+    private int hpTemp;
+    private int speed;
+    private int armour;
+    private final int init;
     
+    // Character Stats
+    private final int strength;
+    private final int dexterity;
+    private final int constitution;
+    private final int intelligence;
+    private final int wisdom;
+    private final int charisma;
+
+    private final int strMod;
+    private final int dexMod;
+    private final int conMod;
+    private final int intMod;
+    private final int wisMod;
+    private final int chaMod;
+
+    // Skills
+    private final int[] skills = new int[18];
+
+    // Inventory
+    private Item equipped;
+    private ArrayList<Item> inventory;
+
+    // Status Effects
+    private ArrayList<String> status;
 }
